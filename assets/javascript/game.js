2aeeb167ded6868e0ce7d82c9cc349275402e30a
youngwoo.cho <petshop0907@gmail.com>
2019-06-23 23:05:50 -0400

    added images of the crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,14 @@
 1. The app generates random number between 19 and 120 and display the number
   <h3 class="rng">
 2. There are 4 crystals. Each crystals are assigned to a random number between 1 and 12.
+  <img class="r-crystal"> ... r b y g-crystal
 3. When user clicks the crystal, the number shows on the score section.
+  <p class="total-score">
 4. Clicking more crystals add the number assigned to that crystal on top of the score.
 5. If user score matches app generated random number, user gets 1 points on win counter.
+  <p class="wins">
 6. If user score exceeds over the app generated random number, user gets 1 points on losses counter.
+  <p class-"losses">
 7. Either the user wins or loses, app generates new random number between 19 - 120.
 8. Either the user wins or loses, each crystals are assigned to a new random number between 1 - 12.
 
