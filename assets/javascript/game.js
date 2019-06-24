49bc47cd6f433e80f63eb83d6d06123ca568637d
youngwoo.cho <petshop0907@gmail.com>
2019-06-23 20:53:05 -0400

    basic html without styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+/* Pseudocoding
+
+1. The app generates random number between 19 and 120 and display the number
+  <h3 class="rng">
+2. There are 4 crystals. Each crystals are assigned to a random number between 1 and 12.
+3. When user clicks the crystal, the number shows on the score section.
+4. Clicking more crystals add the number assigned to that crystal on top of the score.
+5. If user score matches app generated random number, user gets 1 points on win counter.
+6. If user score exceeds over the app generated random number, user gets 1 points on losses counter.
+7. Either the user wins or loses, app generates new random number between 19 - 120.
+8. Either the user wins or loses, each crystals are assigned to a new random number between 1 - 12.
+
+*/
+
